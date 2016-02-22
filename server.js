a330b02fa2a8245c1572ffb07150de368cd15a39
andersonmcook <andersonmcook@gmail.com>
2016-02-22 11:54:52 -0600

    login user with no actual auth

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+'use strict';
+
 const express = require('express');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
+const mongoose = require('mongoose');
 const app = express();
+const userRoutes = require('./lib/user/user.routes');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const SESSION_SECRET = process.env.SESSION_SECRET || 'secretsss';
@@ -20,7 +24,7 @@ app.use(session({
   store: new RedisStore()
 }));
 
-// // middleware
+// // test middleware to count visits to each page per user
 // app.use((req, res, next) => {
 //   req.session.visits = req.session.visits || {};
 //   req.session.visits[req.url] = req.session.visits[req.url] || 0;
@@ -29,39 +33,27 @@ app.use(session({
 //   next();
 // });
 
+app.use((req, res, next) => {
+  app.locals.user = req.session.user || {email: 'Guest'};
+  next();
+});
+
 // get home
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-// get login
-app.get('/login', (req, res) => {
-  res.render('login');
+// user routes
+app.use(userRoutes);
+
+
+// connect
+mongoose.connect('mongodb://localhost:27017/node-authentication', (err) => {
+  if (err) throw err;
+  // listen
+  app.listen(PORT, () => {
+    console.log(`App listening on ${PORT}`);
+  });
+
 });
 
-// post login
-app.post('/login', (req, res) => {
-  res.redirect('/');
-});
-
-// get register
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-// post register
-app.post('/register', (req, res) => {
-  if (req.body.password === req.body.verify) {
-    res.redirect('/login');
-  } else {
-    res.render('register', {
-      email: req.body.email,
-      message: 'Passwords do not match.'
-    });
-  }
-});
-
-// listen
-app.listen(PORT, () => {
-  console.log(`app listening on ${PORT}`);
-});
