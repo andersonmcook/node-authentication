796616310f8d30750a1b469cea23e018cdf1fafa
andersonmcook <andersonmcook@gmail.com>
2016-02-22 09:23:49 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'jade');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`app listening on ${PORT}`);
+});
