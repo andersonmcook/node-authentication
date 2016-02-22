2242af50bc9e5350b36db184ffa6c56ba024b76d
andersonmcook <andersonmcook@gmail.com>
2016-02-22 11:16:29 -0600

    installed redis

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
+const SESSION_SECRET = process.env.SESSION_SECRET || 'secretsss';
 
 // use jade
 app.set('view engine', 'jade');
 
 // body-parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
+
+// session middleware with redis
+// saves req.session in memory
+// run redis with redis-server in CL
+app.use(session({
+  secret: SESSION_SECRET,
+  store: new RedisStore()
+}));
+
+// // middleware
+// app.use((req, res, next) => {
+//   req.session.visits = req.session.visits || {};
+//   req.session.visits[req.url] = req.session.visits[req.url] || 0;
+//   req.session.visits[req.url]++;
+//   console.log(req.session);
+//   next();
+// });
 
 // get home
 app.get('/', (req, res) => {
@@ -34,7 +54,10 @@ app.post('/register', (req, res) => {
   if (req.body.password === req.body.verify) {
     res.redirect('/login');
   } else {
-    res.render('register', {message: 'Passwords do not match.', email: req.body.email});
+    res.render('register', {
+      email: req.body.email,
+      message: 'Passwords do not match.'
+    });
   }
 });
 
