ad8626b5f8b4abe1c3d05323c1bbb4224fa89d1d
andersonmcook <andersonmcook@gmail.com>
2016-02-22 09:46:32 -0600

    added routes and bodyparser

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
+// use jade
 app.set('view engine', 'jade');
 
+// body-parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+// get home
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// get login
+app.get('/login', (req, res) => {
+  res.render('login');
+});
 
+// post login
+app.post('/login', (req, res) => {
+  res.redirect('/');
+});
+
+// listen
 app.listen(PORT, () => {
   console.log(`app listening on ${PORT}`);
 });
